ddbc7acdd1f85fe8460c7d19996f343e1c54522b
Janabai Tukaram Tidke <janatidke2407@gmail.com>
2021-08-31 22:35:07 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace PredictcarOp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car Ford = new Car(22);
+            Ford.Model = 10;
+            Car BMW = Ford;
+            BMW.Display_Model();
+            Ford.Display_Model();
+        }
+
+
+    }
+    class Car
+    {
+        public int Model;
+        public Car(int a)
+        {
+            Model = a;
+        }
+        public void Display_Model()
+        {
+            Console.WriteLine(Model);
+        }
+    }
+}
+
+//op 10,10
+
+
